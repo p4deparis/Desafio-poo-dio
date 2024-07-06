13a04a38a639aecf9b6f8f191f9ba23815162362
Paulo Cesar <pcfernandes017@gmail.com>
2024-07-06 08:43:58 -0300

    desafio

---
+++ br.com.dio.desafio.dominio/src/br/com/dio/desafio/dominio/Conteudo.java
@@ -0,0 +1,43 @@
+
+package br.com.dio.desafio.dominio;
+
+public abstract class Conteudo {
+
+    protected static final double XP_PADRAO = 10d;
+
+    private String titulo;
+    private String descricao;
+
+    /**
+     * @return
+     */
+    public abstract double calcularXp();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    double calcularXp() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    double calcularXp() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public static double getXpPadrao() {
+        return XP_PADRAO;
+    }
+}
